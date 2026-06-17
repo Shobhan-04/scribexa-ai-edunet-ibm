100fd28aa2978af8f214576800941f301de2aa7f
Shobhan Satpathy <165057253+Shobhan-04@users.noreply.github.com>
2026-06-17 10:14:07 +0530

    Implement error handling in ask_gemini
    Add error handling to ask_gemini function

--- modules/gemini_utils.py
+++ modules/gemini_utils.py
@@ -21,5 +21,9 @@ genai.configure(api_key=api_key)
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 def ask_gemini(prompt):
-    response = model.generate_content(prompt)
-    return response.text
+    try:
+        response = model.generate_content(prompt)
+        return response.text
+
+    except Exception as e:
+        return f"ERROR: {str(e)}"
