8a55094946a7a3ec73f47fe6d3287acd9a32ae04
Shobhan Satpathy <165057253+Shobhan-04@users.noreply.github.com>
2026-06-17 16:20:53 +0530

    Update youtube_processor.py

--- modules/youtube_processor.py
+++ modules/youtube_processor.py
@@ -1,28 +1,17 @@
 import yt_dlp
-import os
 
 def download_audio(url):
 
-    os.makedirs("downloads", exist_ok=True)
-
-    output_path = "downloads/lecture.%(ext)s"
-
     ydl_opts = {
         "format": "bestaudio/best",
-        "outtmpl": output_path,
-        "quiet": True,
+        "outtmpl": "downloads/audio.%(ext)s",
+        "quiet": True
     }
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    with yt_dlp.YoutubeDL(
+        ydl_opts
+    ) as ydl:
+
         ydl.download([url])
 
-    for file in os.listdir("downloads"):
-
-        if file.startswith("lecture"):
-
-            return os.path.join(
-                "downloads",
-                file
-            )
-
-    return None
+    return "downloads/audio.webm"
