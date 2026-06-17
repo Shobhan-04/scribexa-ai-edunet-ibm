720f86c061d755d7d1849c03419207ce74c3accc
Shobhan Satpathy <165057253+Shobhan-04@users.noreply.github.com>
2026-06-17 15:56:36 +0530

    Update exam_predictor.py

--- modules/exam_predictor.py
+++ modules/exam_predictor.py
@@ -1,4 +1,4 @@
-from modules.gemini_utils import ask_gemini
+from modules.groq_utils import ask_groq
 
 def predict_exam_questions(
     text
@@ -13,4 +13,4 @@ def predict_exam_questions(
     {text}
     """
 
-    return ask_gemini(prompt)
+    return ask_groq(prompt)
