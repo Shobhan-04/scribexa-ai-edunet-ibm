8e1449bd20c388394bbba264b8411a9b2926cb87
Shobhan Satpathy <165057253+Shobhan-04@users.noreply.github.com>
2026-06-17 16:20:19 +0530

    Update whisper_utils.py

--- modules/whisper_utils.py
+++ modules/whisper_utils.py
@@ -3,5 +3,9 @@ import whisper
 model = whisper.load_model("base")
 
 def transcribe_audio(audio_path):
-    result = model.transcribe(audio_path)
+
+    result = model.transcribe(
+        audio_path
+    )
+
     return result["text"]
