e739cfe3f08829d54647f33967999ad78ad43dcb
Shobhan Satpathy <165057253+Shobhan-04@users.noreply.github.com>
2026-06-17 09:43:04 +0530

    Added youtube processor
    Refactor download_audio function to create downloads directory and update output path.

--- modules/youtube_processor.py
+++ modules/youtube_processor.py
@@ -1,11 +1,28 @@
 import yt_dlp
+import os
 
 def download_audio(url):
 
-    options = {
-        "format": "bestaudio",
-        "outtmpl": "uploads/youtube_audio/%(title)s.%(ext)s"
+    os.makedirs("downloads", exist_ok=True)
+
+    output_path = "downloads/lecture.%(ext)s"
+
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": output_path,
+        "quiet": True,
     }
 
-    with yt_dlp.YoutubeDL(options) as ydl:
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
+
+    for file in os.listdir("downloads"):
+
+        if file.startswith("lecture"):
+
+            return os.path.join(
+                "downloads",
+                file
+            )
+
+    return None
