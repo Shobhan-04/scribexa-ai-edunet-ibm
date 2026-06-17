65ec7d9d7286dcdcd73db3835ffa5abb3c0342f8
Shobhan Satpathy <165057253+Shobhan-04@users.noreply.github.com>
2026-06-17 10:45:41 +0530

    Update gemini_utils.py

--- modules/gemini_utils.py
+++ modules/gemini_utils.py
@@ -18,7 +18,9 @@ if not api_key:
 
 genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel(
+    "gemini-1.5-flash"
+)
 
 def ask_gemini(prompt):
     try:
